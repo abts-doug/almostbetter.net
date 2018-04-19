cf61603409932e74e61421ea523ec46f25c2e2c1
abts-doug <douglasjamescoleman@gmail.com>
2018-04-18 20:58:47 -0400

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -3,6 +3,7 @@ exports.RSS_FEEDS = {
     'abtd': 'https://pinecast.com/feed/abtd',
     'ect': 'https://pinecast.com/feed/erie-canal-theatre',
     'lio': 'https://pinecast.com/feed/life-in-overdrive',
-    'pcp': 'http://presscontinue.podbean.com/feed/',
+    'pcp': 'http://pinecast.com/feed/press-continue-podcast',
+    'pnol': 'http://pinecast.com/feed/paper-news-out-loud',
     'tsk': 'https://pinecast.com/feed/tsk',
 };
